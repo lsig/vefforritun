cad88f456f9b8ad0297e2b113fec47528f45d064
Lsig <logis21@ru.is>
2023-03-30 13:52:47 +0000

    two tests added for failing patch and get

--- prj04/proj03_solution-modified/test/index.test.js
+++ prj04/proj03_solution-modified/test/index.test.js
@@ -33,6 +33,70 @@ describe("Endpoint tests", () => {
     expect(content).toBeInstanceOf(Array);
   });
 
+  test("GET individual tune fails, id not found", async () => {
+    const res = await request(apiUrl).get("/api/v1/genres/0/tunes/10");
+    expect(res.status).toBe(404);
+    expect(res.body).toBeDefined();
+    expect(res.body.message).toBe("Tune with id 10 does not exist.");
+  });
+
+  test("PATCH individual tunes", async () => {
+    const updatedTune = {
+      name: "Ave Maria",
+      genreId: "1",
+      content: [
+        {
+          note: "E5",
+          duration: "8n",
+          timing: 0,
+        },
+      ],
+    };
+
+    const res = await request(apiUrl)
+      .patch("/api/v1/genres/0/tunes/3")
+      .send(updatedTune);
+
+    const { id, name, genreId, content } = res.body;
+
+    expect(res.status).toBe(200);
+    expect(res.body).toBeDefined();
+    expect(id).toEqual("3");
+    expect(name).toEqual("Ave Maria");
+    expect(genreId).toEqual("1");
+    expect(content).toBeInstanceOf(Array);
+    expect(content).toEqual([
+      {
+        note: "E5",
+        duration: "8n",
+        timing: 0,
+      },
+    ]);
+  });
+
+  test("PATCH individual tunes fails since genreId is wrong", async () => {
+    const updatedTune = {
+      name: "Ave Maria",
+      genreId: "1",
+      content: [
+        {
+          note: "E5",
+          duration: "8n",
+          timing: 0,
+        },
+      ],
+    };
+
+    const res = await request(apiUrl)
+      .patch("/api/v1/genres/2/tunes/3")
+      .send(updatedTune);
+
+    expect(res.status).toBe(400);
+    expect(res.body).toBeDefined();
+    expect(res.body.message).toEqual(
+      "Tune with id 3 does not have genre id 2."
+    );
+  });
   // Do something weird
   test("GET /randomURL causes 405", async () => {
     const res = await request(apiUrl).get("/api/v1/randomUrl");
