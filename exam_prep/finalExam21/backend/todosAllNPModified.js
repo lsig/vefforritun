e56ca33405570418f74ae49c96d6248cc9054cf7
Lsig <logis21@ru.is>
2023-04-12 22:05:14 +0000

    finalexam21 completed

--- exam_prep/finalExam21/backend/todosAllNPModified.js
+++ exam_prep/finalExam21/backend/todosAllNPModified.js
@@ -1,46 +1,96 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
 
 const prefix = "/api";
 const version = "/vEx0";
+let nextNoteId = 3;
 
 app.use(express.json());
 app.use(cors());
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-let notes = [{ id: 10, name: "todos for today", content: "Prepare Lab 6", priority: 1 }, { id: 12, name: "memo for l15", content: "Do not forget to mention Heroku", priority: 5 }];
+let notes = [
+  { id: 1, name: "todos for today", content: "Prepare Lab 6", priority: 1 },
+  {
+    id: 2,
+    name: "memo for l15",
+    content: "Do not forget to mention Heroku",
+    priority: 5,
+  },
+];
 
 module.exports.resetServerState = function () {
-    //Does nothing
+  //Does nothing
 };
 
-app.get(prefix + version + '/notes', (req, res) => {
-    return res.status(200).json(notes);
+app.get(prefix + version + "/notes", (req, res) => {
+  const { greater_than } = req.query;
+  if (greater_than && !isNaN(greater_than)) {
+    const greaterArray = notes.filter(
+      (note) => note.id > parseInt(greater_than)
+    );
+    return res.status(200).json(greaterArray);
+  }
+  return res.status(200).json(notes);
 });
 
-app.delete(prefix + version + '/notes/:noteId', (req, res) => {
-    for (let i = 0; i < notes.length; i++) {
-        if (notes[i].id == req.params.noteId) {
-            res.status(200).json(notes.splice(i, 1)[0]);
-            return;
-        }
+app.post(prefix + version + "/notes", (req, res) => {
+  const { name, content, priority } = req.body;
+  if (!name || !content || !priority) {
+    return res
+      .status(400)
+      .send({ message: "A note must have a name, content and priority" });
+  }
+  if (
+    typeof name !== "string" ||
+    typeof content !== "string" ||
+    typeof priority !== "number"
+  ) {
+    return res.status(400).send({
+      message:
+        "A note must have valid properties, a name which is string, content which is a string and a priority which is a number",
+    });
+  }
+
+  const newNote = {
+    id: nextNoteId,
+    name: name,
+    content: content,
+    priority: priority,
+  };
+  notes.push(newNote);
+  nextNoteId++;
+  res.status(201).json(newNote);
+});
+
+app.delete(prefix + version + "/notes/:noteId", (req, res) => {
+  for (let i = 0; i < notes.length; i++) {
+    if (notes[i].id == req.params.noteId) {
+      res.status(200).json(notes.splice(i, 1)[0]);
+      return;
     }
-    res.status(404).json({ 'message': "Note with id " + req.params.noteId + " does not exist." });
+  }
+  res.status(404).json({
+    message: "Note with id " + req.params.noteId + " does not exist.",
+  });
 });
 
 //Default: Not supported
-app.use('*', (req, res) => {
-    res.status(405).send('Operation not supported.');
+app.use("*", (req, res) => {
+  res.status(405).send("Operation not supported.");
 });
 
 app.listen(port, () => {
-    console.log('Todo note app listening on port ' + port);
+  console.log("Todo note app listening on port " + port);
 });
--- exam_prep/finalExam21/frontend/main.js
+++ exam_prep/finalExam21/frontend/main.js
@@ -1,25 +1,56 @@
 function getAllTodos() {
-    var noteList = document.getElementById("noteList");
-    noteList.innerHTML='';
+  const noteList = document.getElementById("noteList");
+  noteList.innerHTML = "";
 
-    var url = 'http://localhost:3000/api/vEx0/notes';
+  const url = "http://localhost:3000/api/vEx0/notes";
 
-    axios.get(url)
-        .then(function (response) {
-            if (response.data !== null) {
-                for (var i = 0; i < response.data.length; i++) {
-                    var noteItem = document.createElement("p");
-                    var noteText = document.createTextNode("Name: " + response.data[i].name + ", Content: " + response.data[i].content + " " + ", Prio: " + response.data[i].priority + "\n");
-                    noteItem.appendChild(noteText);
-                    noteList.appendChild(noteItem);
-                }
-            }
-        })
-        .catch(function (error) {
-            //When unsuccessful, print the error.
-            console.log(error);
+  axios
+    .get(url)
+    .then(function (response) {
+      if (response.data !== null) {
+        response.data.forEach((noteData) => {
+          const note = createNote(noteData);
+          const deleteButton = createDeleteButton(note, noteData.id, url);
+          note.appendChild(deleteButton);
+          noteList.appendChild(note);
         });
-
+      }
+    })
+    .catch(function (error) {
+      //When unsuccessful, print the error.
+      console.log(error);
+    });
 }
+
+const createNote = (noteData) => {
+  const note = document.createElement("div");
+  note.setAttribute("class", "note");
+  note.setAttribute("id", `${noteData.id}`);
+  const noteName = createNoteParagraph(`Name: ${noteData.name},`);
+  const noteContent = createNoteParagraph(`Content: ${noteData.content},`);
+  const notePrior = createNoteParagraph(`Prio: ${noteData.priority}`);
+  note.appendChild(noteName);
+  note.appendChild(noteContent);
+  note.appendChild(notePrior);
+  return note;
+};
+
+const createNoteParagraph = (text) => {
+  const noteParagraph = document.createElement("p");
+  const noteText = document.createTextNode(text);
+  noteParagraph.appendChild(noteText);
+  return noteParagraph;
+};
+
+const createDeleteButton = (note, noteId, url) => {
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "Delete Button";
+  note.appendChild(deleteButton);
+
+  deleteButton.addEventListener("click", () => {
+    axios.delete(url + `/${noteId}`).then(() => getAllTodos());
+  });
+  return deleteButton;
+};
 
 getAllTodos();
